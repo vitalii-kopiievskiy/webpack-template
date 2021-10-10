6e58b538ba11dcd1eb1e3c46f2ba0238c173e96d
vitalii-kopiievskiy <VBN$%^vbn456>
2021-10-10 15:41:47 +0300

    Add scripts, environment variables, mode property. Install webpack-dev-server.

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+let mode = 'development'
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+console.log(mode + ' mode')
+
+module.exports = {
+    mode: mode,
+    plugins: [],
+    module: {
+        rules: []
+    },
+}
