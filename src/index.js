25767b42786e42cf3b2080982be22e43d88c572a
vitalii-kopiievskiy <VBN$%^vbn456>
2021-10-11 19:45:12 +0300

    Install and configuration styles` loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss'
+
 const userStack = {
     language: 'JavaScript',
     framework: 'Angular'
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 
 let mode = 'development'
 if (process.env.NODE_ENV === 'production') {
@@ -9,10 +11,37 @@ console.log(mode + ' mode')
 module.exports = {
     mode: mode,
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
         new HtmlWebpackPlugin({
-        template: "./src/index.html"
-    })],
+            template: "./src/index.html"
+        })],
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    "sass-loader",
+                ],
+            },
+        ]
     },
 }
